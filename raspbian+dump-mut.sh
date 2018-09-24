fb52cdb0b4ab348dc3787f8a499689a24f374877
abcd567a <abcd567@hotmail.com>
2018-09-24 11:45:03 -0400

    Update raspbian+dump-mut.sh

--- raspbian+dump-mut.sh
+++ raspbian+dump-mut.sh
@@ -22,7 +22,7 @@ sudo /etc/init.d/lighttpd force-reload
 
 
 echo "Creating file gain.php...."
-FILE_GAIN="/usr/share/dump1090-motability/html/gain.php"
+FILE_GAIN="/usr/share/dump1090-mutability/html/gain.php"
 sudo touch $FILE_GAIN
 
 echo "making file gain.php writeable by script (666)...."
